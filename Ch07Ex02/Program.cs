1e41a5b69038622b68764085c5480460a7aac260
dmchub <37676729+dmchub@users.noreply.github.com>
2020-04-12 01:59:51 +0300

    Добавьте файлы проекта.

---
+++ Ch07Ex02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+
+namespace Ch07Ex02
+{
+    class Program
+    {
+        static string[] eTypes = { "none", "simple", "index", "nested index", "filter" };
+        static void Main(string[] args)
+        {
+            foreach(string eType in eTypes)
+            {
+                try
+                {
+                    WriteLine("Main() try block reached. "); // Line 21
+                    WriteLine($"ThrowException(\"{eType}\") called.");
+                    ThrowException(eType);
+                }
+            }
+        }
+    }
+}
